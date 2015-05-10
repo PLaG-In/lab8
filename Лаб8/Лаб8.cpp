49a0dcc0418e9a45186efa00b4e6daa9979d1055
PLaGInc <vladimir.alt@inbox.ru>
2015-05-10 15:09:00 +0400

    Доделал

--- Лаб8/Лаб8.cpp
+++ Лаб8/Лаб8.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 string delete_line;
 vector <__int64> vec_del;
-__int64 g_freq, show_time, find_time; // глобальная переменная
+__int64 g_freq, g_showTime, g_findTime, g_summVecTime; // глобальная переменная
 
 void Init()
 {
@@ -31,11 +31,13 @@ __int64 GetMicroTickCount()		// время в mcs
 
 __int64 SummVec(vector<__int64> vect)
 {
+	__int64 begin = GetMicroTickCount();
 	__int64 summ = 0;
 	for (size_t i = 0; i < vect.size(); i++)
 	{
 		summ = summ + vect[i];
 	}
+	g_summVecTime = GetMicroTickCount() - begin;
 	return summ;
 }
 
@@ -71,7 +73,7 @@ map<string, int> FindCommonWord(string line)
 		}
 		count++;
 	} while (count < line.size());
-	find_time = GetMicroTickCount() - begin;
+	g_findTime = GetMicroTickCount() - begin;
 	return Dictionary;
 }
 
@@ -97,11 +99,11 @@ char* ShowDictionary(map<string, int> Dictionary)
 	}
 	Result = new char[Key.length() + 1];
 	strcpy_s(Result, Key.length() + 1, Key.c_str());
-	show_time = GetMicroTickCount() - begin;
+	g_showTime = GetMicroTickCount() - begin;
 	return Result;
 }
 
-char* Deleted_Comment(char* str)
+char* DeletedComment(char* str)
 {
 	__int64 begin = GetMicroTickCount();
 	assert(str);
@@ -161,6 +163,7 @@ char* Deleted_Comment(char* str)
 			*CopySymbol++;
 		*CommentSymbol++;
 	}
+	cout << begin;
 	vec_del.push_back(GetMicroTickCount() - begin);
 	return t;
 }
@@ -205,7 +208,7 @@ int main(int argc, char* argv[])
 			text += "\n";
 			cstr = new char[text.length() + 1];
 			strcpy_s(cstr, text.length() + 1, text.c_str());
-			Deleted_Comment(cstr);
+			DeletedComment(cstr);
 			a.push_back(cstr);
 			text = "";
 		}
@@ -227,6 +230,7 @@ int main(int argc, char* argv[])
 
 	map<string, int> MassOfWord;
 	MassOfWord = FindCommonWord(delete_line);
+	system("cls");
 	__int64 summ = SummVec(vec_del);
 	__int64 average = summ / vec_del.size();
 	//for (size_t i = 0; i < vec_del.size(); i++)
@@ -237,10 +241,11 @@ int main(int argc, char* argv[])
 	input.close();
 	__int64 res = (GetMicroTickCount() - begin);
 	cout << "The most used word is: " << ShowDictionary(MassOfWord) << endl;
-	cout << "Total time of ShowDictionary function(mcs) : " << show_time << endl;
-	cout << "Total time of FindCommonWord function(mcs) : " << find_time << endl;
-	cout << "Total time of Deleted_Comment function(mcs): " << summ << endl;
-	cout << "Average time of Deleted_Comment function(mcs): " << average << endl;
+	cout << "Total time of ShowDictionary function(mcs) : " << g_showTime << endl;
+	cout << "Total time of FindCommonWord function(mcs) : " << g_findTime << endl;
+	cout << "Total time of DeletedComment function(mcs): " << summ << endl;
+	cout << "Average time of DeletedComment function(mcs): " << average << endl;
+	cout << "Total time of SummVec function(mcs) : " << g_summVecTime << endl;
 	cout << "Total time of program(mcs): " << res << endl;
 	system("pause");
 	return 0;
